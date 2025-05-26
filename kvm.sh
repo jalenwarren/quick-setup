c2571fed72068b47f2e2ed7c4bc537256da35d13
Jalen Warren <jalenwarren@utexas.edu>
2025-05-26 17:04:44 +0000

    Update kvm.sh
    completed initial setup

--- kvm.sh
+++ kvm.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-#KVM Setup
+#install
+sudo apt install --ignore-missing qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
+
+#Set virtualization service to autostart, restart it, check it 
+sudo systemctl enable libvirtd
+sudo systemctl restart libvirtd
+sudo systemctl status libvirtd 
+
+#Final check that KVM is loaded. You should get something like "kvm_intel" or "kvm_amd"
+sudo lsmod | grep -i kvm 
+
+#add to KVM group
+sudo usermod -aG libvirt <username>
+sudo usermod -aG kvm <username>
