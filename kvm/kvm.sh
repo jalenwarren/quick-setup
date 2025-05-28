c5c96728635109835c68018e2b9a80b3bde06528
Jalen Warren <jalenwarren@utexas.edu>
2025-05-27 20:07:44 -0500

    updated quicksetup

--- kvm/kvm.sh
+++ kvm/kvm.sh
@@ -14,3 +14,12 @@ sudo lsmod | grep -i kvm
 #add to KVM group
 sudo usermod -aG libvirt <username>
 sudo usermod -aG kvm <username>
+# Adjust permissions
+sudo chmod 660 /var/run/libvirt/libvirt-sock
+sudo chown root:libvirt /var/run/libvirt/libvirt-sock
+
+#Configure libvirt to Allow Group Access
+sudo nano /etc/libvirt/libvirtd.conf
+#Find and set the following parameters:
+unix_sock_group = "libvirt"
+unix_sock_rw_perms = "0770"
